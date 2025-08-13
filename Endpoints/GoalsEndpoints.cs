90ff326456050456418013b57fd3a076be8bbc96
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2025-08-13 23:47:15 +0400

    Update GoalsEndpoints.cs

--- Endpoints/GoalsEndpoints.cs
+++ Endpoints/GoalsEndpoints.cs
@@ -1,9 +1,24 @@
-using Project_Backend_2024.DTO.TechStack;
-using BudgetApp2.Domain;
-namespace BudgetApp2.Endpoints;
-public static class GoalsEndpoints{
-  public static void Map(object app){
-    TechStack.Map(app,"GET","/goals",()=>new object[0]);
-    TechStack.Map(app,"POST","/goals",(Goal g)=>g);
-  }
+using Project_Backend_2024.DTO;
+
+namespace Project_Backend_2024.Endpoints;
+
+public static class GoalsEndpoints
+{
+    private static readonly List<Goal> _goals = new()
+    {
+        new Goal { Title = "Vacation", Desctiption = "My 2 week vacation in Ibiza" }
+    };
+
+    public static void MapGoalsEndpoints(this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup("/goals");
+
+        group.MapGet("/", () => Results.Ok(_goals));
+
+        group.MapPost("/", (Goal goal) =>
+        {
+            _goals.Add(goal);
+            return Results.Created($"/goals/{_goals.Count - 1}", goal);
+        });
+    }
 }
