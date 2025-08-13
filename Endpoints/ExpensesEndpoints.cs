011c90c4c3db98fd92c516712af0a8b1b0a9945f
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2025-08-13 23:45:24 +0400

    Update ExpensesEndpoints.cs

--- Endpoints/ExpensesEndpoints.cs
+++ Endpoints/ExpensesEndpoints.cs
@@ -1,9 +1,25 @@
-using Project_Backend_2024.DTO.TechStack;
-using BudgetApp2.DTO;
-namespace BudgetApp2.Endpoints;
-public static class ExpensesEndpoints{
-  public static void Map(object app){
-    TechStack.Map(app,"GET","/expenses",()=>new object[0]);
-    TechStack.Map(app,"POST","/expenses",(ExpenseDto dto)=>dto);
-  }
+using Project_Backend_2024.DTO;
+
+namespace Project_Backend_2024.Endpoints;
+
+public static class ExpensesEndpoints
+{
+    private static readonly List<Expense> _expenses = new()
+    {
+        new Expense { Id = "123", Amount: 100, Date: DateTime.Now },
+        new Expense { Id = "123", Amount: 70, Date: DateTime.Now }
+    };
+
+    public static void MapExpensesEndpoints(this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup("/expenses");
+
+        group.MapGet("/", () => Results.Ok(_expenses));
+
+        group.MapPost("/", (TechStack expense) =>
+        {
+            _expenses.Add(expense);
+            return Results.Created($"/expenses/{_expenses.Count - 1}", expense);
+        });
+    }
 }
