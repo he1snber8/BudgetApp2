5ce160e700cc33ea425042d3b85c2a84f331ddde
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2025-08-13 22:36:25 +0400

    Update ExpenseDto.cs

--- DTO/ExpenseDto.cs
+++ DTO/ExpenseDto.cs
@@ -1,9 +1,4 @@
 using Project_Backend_2024.DTO.TechStack;
 namespace BudgetApp2.DTO;
 
-public class Expense {
-  public int Id { get;set;}
-  public decimal Amount { get;set;}
-  public Date TransactionDate { get;set;}
-  public string Merchant { get;set;}
-}
+public record ExpenseDto(string Category, decimal Amount, DateTime Date, string Notes="");
